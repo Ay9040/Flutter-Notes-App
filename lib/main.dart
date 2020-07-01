4917e1bc1e85c403188ce6db5d91a6b5dc34e869
Ayush Khade <ayush.khade04@outlook.com>
2020-07-02 00:04:29 +0530

    added firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final database = Firestore.instance;
 
 void main() {
   runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -13,122 +14,140 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-        scaffoldBackgroundColor: Colors.grey[900],
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: ThemeData.dark(),
       home: HomePage(),
     );
   }
 }
 
-
-
-class HomePage extends StatefulWidget{
-
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  var currentIndex = -1;
 
-  var notes = ['Hello World', 'Second Note'];
-  var colors = [Colors.red[200], Colors.blue[200], Colors.green[200], Colors.orange[200], Colors.indigo[200]];
-  var borderColors = [Colors.red[400], Colors.blue[400], Colors.green[400], Colors.orange[400], Colors.indigo[400]];
+  var colors = [
+    Colors.red[200],
+    Colors.blue[200],
+    Colors.green[200],
+    Colors.orange[200],
+    Colors.indigo[200]
+  ];
+  var borderColors = [
+    Colors.red[400],
+    Colors.blue[400],
+    Colors.green[400],
+    Colors.orange[400],
+    Colors.indigo[400]
+  ];
 
-  editNote(int index, var note){
-    notes[index] = note.toString();
+  editNote(var index, var note) async {
+    try {
+      await database
+          .collection('notes')
+          .document(index.toString())
+          .updateData({'note': note});
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
-  newNote(var note){
-    notes.add(note.toString());
-  }
-
-  deleteNote(var index){
-    notes.removeAt(index);
+  newNote(var note, var index) async {
+    await database
+        .collection("notes")
+        .document(index.toString())
+        .setData({'note': note});
+    currentIndex = currentIndex + 1;
     setState(() {});
   }
 
-  createDeleteDialog(BuildContext context, var index){
-    return showDialog(context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                        content: Text('Delete this note?'),
-                        actions: <Widget>[
-                          MaterialButton(
-                            child: Text('Yes'),
-                            color: Colors.blue,
-                            onPressed: () {
-                              deleteNote(index);
-                              setState(() {});
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          MaterialButton(
-                            child: Text('No'),
-                            color: Colors.red,
-                            onPressed: (){
-                              Navigator.of(context).pop();
-                            },
-                          )
-                        ],
-                        backgroundColor: colors[Random().nextInt(colors.length)],
-                      );
-      }
-    );
+  deleteNote(var index) {
+    database.collection('notes').document(index.toString()).delete();
   }
 
-  createNotesDialog(BuildContext context, {var index:-1}){
-
-    TextEditingController notesController = TextEditingController(text: index == -1? "": notes[index].toString());
-
-    return showDialog(context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          content: EditableText(
-                                    minLines: 4,
-                                    maxLines: 20,
-                                    autofocus: true,
-                                    controller: notesController,
-                                    cursorColor: Colors.black,
-                                    focusNode: FocusNode(),
-                                    keyboardType: TextInputType.multiline,
-                                    backgroundCursorColor: Colors.black,
-                                    style: TextStyle(fontSize: 14, 
-                                                    color: Colors.black),
-                                    ),
-                          actions: <Widget>[
-                            MaterialButton(
-                              child: Text('Save'),
-                              color: Colors.green,
-                              height: 60,
-                              minWidth: 80,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-                              onPressed: () {
-                                index == -1 ? newNote(notesController.value.text): editNote(index, notesController.value.text);
-                                setState(() {});
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                            MaterialButton(
-                              child: Text('Cancel'),
-                              color: Colors.red,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-                              height: 60,
-                              minWidth: 80,
-                              onPressed: (){
-                                Navigator.of(context).pop();
-                              },
-                            )
-                          ],
-                          backgroundColor: colors[Random().nextInt(colors.length)],
-                        );
-      }
-    );
+  createDeleteDialog(BuildContext context, var index) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text('Delete this note?'),
+            actions: <Widget>[
+              MaterialButton(
+                child: Text('Yes'),
+                color: Colors.blue,
+                onPressed: () {
+                  deleteNote(index);
+                  Navigator.of(context).pop();
+                },
+              ),
+              MaterialButton(
+                child: Text('No'),
+                color: Colors.red,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+            backgroundColor: colors[Random().nextInt(colors.length)],
+          );
+        });
   }
 
+  createNotesDialog(BuildContext context, {var index: 0}) {
+    TextEditingController notesController = TextEditingController(
+        text: index > currentIndex
+            ? ""
+            : database.collection('notes').document(index).get().toString());
 
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: EditableText(
+              minLines: 4,
+              maxLines: 20,
+              autofocus: true,
+              controller: notesController,
+              cursorColor: Colors.black,
+              focusNode: FocusNode(),
+              keyboardType: TextInputType.multiline,
+              backgroundCursorColor: Colors.black,
+              style: TextStyle(fontSize: 14, color: Colors.black),
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                child: Text('Save'),
+                color: Colors.green,
+                height: 60,
+                minWidth: 80,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+                onPressed: () {
+                  index > currentIndex
+                      ? newNote(notesController.value.text, index)
+                      : editNote(index, notesController.value.text);
+                  setState(() {});
+                  Navigator.of(context).pop();
+                },
+              ),
+              MaterialButton(
+                child: Text('Cancel'),
+                color: Colors.red,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+                height: 60,
+                minWidth: 80,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+            backgroundColor: colors[Random().nextInt(colors.length)],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -136,37 +155,57 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Notes'),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      body: StreamBuilder(
+          stream: database.collection('notes').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return Center(child: CircularProgressIndicator());
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 2,
                 mainAxisSpacing: 2,
               ),
-        
-        itemBuilder: (context, index) {
-        var colorIndex = Random().nextInt(colors.length);
-        return Container(child: DecoratedBox(
-                                  decoration: BoxDecoration(border: Border.all(color: borderColors[colorIndex], width: 10), borderRadius: BorderRadius.all(Radius.circular(8))),
-                                  child: Card(
-                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-                                    color: colors[colorIndex],
-                                    child: ListTile(title: Text(notes[index]), 
-                                                                      onTap: (){createNotesDialog(context, index:index);}, 
-                                                                      onLongPress: () {createDeleteDialog(context, index);},
-                                                                    ),
-                                  ),
-        ),
-                        margin: const EdgeInsets.all(8),);
-      },
-      itemCount: notes.length,
-      ),
+              itemBuilder: (context, index) {
+                var colorIndex = Random().nextInt(colors.length);
+                return Container(
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: borderColors[colorIndex], width: 10),
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                      color: colors[colorIndex],
+                      child: ListTile(
+                        title: Text(
+                          snapshot.data.documents[index]['note'],
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onTap: () {
+                          createNotesDialog(context,
+                              index: snapshot.data.documents[index].documentId);
+                        },
+                        onLongPress: () {
+                          createDeleteDialog(context,
+                              snapshot.data.documents[index].documentId);
+                        },
+                      ),
+                    ),
+                  ),
+                  margin: const EdgeInsets.all(8),
+                );
+              },
+              itemCount: snapshot.data.documents.length,
+            );
+          }),
       floatingActionButton: FloatingActionButton(
-                              onPressed: (){
-                                createNotesDialog(context);
-                              },
-                              child: Icon(Icons.add),
+        onPressed: () {
+          createNotesDialog(context, index: (currentIndex + 1));
+        },
+        child: Icon(Icons.add),
       ),
-      
     );
   }
 }
